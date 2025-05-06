c5a3645e95eac2536ce171dcf4e3b971ce8c61ac
Irene Riportella <riportellairene@gmail.com>
2025-05-06 18:23:30 +0200

    style:💄 Add fixed width and height

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
             >
               <div className="flex justify-between items-center">
                 <h2 className="text-xl font-bold my-2">{question.question}</h2>
-                <button className="border-3 border-blue-800 rounded-md bg-blue-200 px-2  cursor-pointer font-bold text-2xl text-blue-800">
+                <button className="border-3 border-blue-800 rounded-md bg-blue-200 px-2  cursor-pointer font-bold text-2xl text-blue-800 w-10 h-10">
                   +
                 </button>
               </div>
