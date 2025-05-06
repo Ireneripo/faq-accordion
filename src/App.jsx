f60d4bd8b7540460e17963e95f22ac6d5ed75115
Irene Riportella <riportellairene@gmail.com>
2025-05-06 21:26:23 +0200

    feat:✨ Add toggle logic

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,12 @@ const questions = [
 ];
 
 function App() {
+  const [expandedId, setExpandedId] = React.useState(null);
+
+  function toggleQuestion(id) {
+    setExpandedId(expandedId === id ? null : id);
+  }
+
   return (
     <>
       <h1 className="text-center text-4xl my-8 font-bold">
@@ -43,19 +49,26 @@ function App() {
         <p className="text-white text-3xl font-bold text-center">
           Frequently Asked Questions
         </p>
-        {questions.map((question, index) => {
+        {questions.map((question, id) => {
           return (
             <div
-              key={index}
+              key={id}
               className="bg-white my-6 p-4 shadow-lg shadow-gray-800"
             >
               <div className="flex justify-between items-center">
                 <h2 className="text-xl font-bold my-2">{question.question}</h2>
-                <button className="border-3 border-blue-800 rounded-md bg-blue-200 px-2  cursor-pointer font-bold text-2xl text-blue-800 w-10 h-10">
-                  +
+                <button
+                  className="border-3 border-blue-800 rounded-md bg-blue-200 px-2  cursor-pointer font-bold text-2xl text-blue-800 w-10 h-10"
+                  onClick={() => {
+                    toggleQuestion(question.id);
+                  }}
+                >
+                  {expandedId === question.id ? "-" : "+"}
                 </button>
               </div>
-              <p className="text-gray-600 text-lg">{question.answer}</p>
+              {expandedId === question.id && (
+                <p className="text-gray-600 text-lg">{question.answer}</p>
+              )}
             </div>
           );
         })}
